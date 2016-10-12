53f4e023eac050e61e0e06e060b11680b9c920f6
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2016-10-12 13:10:57 +0200

    Initial commit with Spring Initializr

---
+++ src/main/java/org/lafeuille/ljcp/LjcpApiApplication.java
@@ -0,0 +1,12 @@
+package org.lafeuille.ljcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjcpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LjcpApiApplication.class, args);
+	}
+}
