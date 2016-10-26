3b150360754c4b36da0a7a512baca287beabb93c
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2016-10-26 07:39:39 +0200

    Add Json Test

--- src/main/java/org/lafeuille/ljcp/core/event/Event.java
+++ src/main/java/org/lafeuille/ljcp/core/event/Event.java
@@ -33,16 +33,32 @@ public class Event extends JpaBase {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public LocalDate getStartDate() {
         return startDate == null ? null : startDate.toLocalDate();
     }
 
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate == null ? null : Date.valueOf(startDate);
+    }
+
     public LocalTime getStartTime() {
         return startTime == null ? null : startTime.toLocalTime();
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime == null ? null : Time.valueOf(startTime);
     }
 
     /**
---
+++ src/test/java/org/lafeuille/ljcp/core/event/EventJsonTest.java
@@ -0,0 +1,35 @@
+package org.lafeuille.ljcp.core.event;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+public class EventJsonTest {
+
+    @Autowired
+    private JacksonTester<Event> json;
+
+    @Test
+    public void foobar() throws IOException {
+        Event event = new Event();
+        event.setTitle("My birthday");
+        event.setDescription("Party for my birthday");
+        event.setStartDate(LocalDate.of(1982, Month.AUGUST, 7));
+        event.setStartTime(LocalTime.of(18, 30));
+        assertThat(json.write(event)).isEqualToJson("{title:'My birthday', description:'Party for my birthday'}");
+        //assertThat(json.write(event)).isEqualToJson("{title:'My birthday', description:'Party for my birthday', startDate:'1982-08-07'}");
+    }
+
+}
