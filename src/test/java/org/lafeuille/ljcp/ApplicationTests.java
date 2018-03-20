4e74dd0f13f97c7aec0da9924d03bb4b226b52f8
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2018-03-20 22:53:51 +0100

    Added version in info endpoint

--- src/test/java/org/lafeuille/ljcp/ApplicationTests.java
+++ src/test/java/org/lafeuille/ljcp/ApplicationTests.java
@@ -5,15 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
-import java.time.Period;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
     @Test
-    public void application() throws Exception {
+    public void application() {
     }
 
 }
