a742e23ee85be9f11d82615b9fc2283ea81ef8dc
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2018-11-28 13:39:01 +0100

    Add automatic Logger injection solver

--- src/main/java/org/lafeuille/ljcp/infra/InfraConfig.java
+++ src/main/java/org/lafeuille/ljcp/infra/InfraConfig.java
@@ -1,11 +1,17 @@
 package org.lafeuille.ljcp.infra;
 
 import com.github.javafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import java.time.Clock;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @Configuration
 public class InfraConfig {
@@ -19,6 +25,12 @@ public class InfraConfig {
     @Bean
     Clock clock() {
         return Clock.systemUTC();
+    }
+
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    Logger logger(InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
     }
 
 }
