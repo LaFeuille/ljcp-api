e657938f7f2570f85a9174328a99245a1e8313b8
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2019-03-11 08:24:35 +0100

    Security

--- src/main/java/org/lafeuille/ljcp/rest/SecurityConfig.java
+++ src/main/java/org/lafeuille/ljcp/rest/SecurityConfig.java
@@ -2,13 +2,13 @@ package org.lafeuille.ljcp.rest;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@ConditionalOnProperty({"auth0.api-audience", "auth0.issuer"})
 @Configuration
-@ConditionalOnClass(JwtWebSecurityConfigurer.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${auth0.api-audience}")
