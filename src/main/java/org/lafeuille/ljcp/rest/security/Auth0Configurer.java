d2107ab6ceaecbb4ffdf9019f5c0cbe4f42e8414
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2019-06-05 22:39:23 +0200

    Disabling CSRF protection

--- src/main/java/org/lafeuille/ljcp/rest/security/Auth0Configurer.java
+++ src/main/java/org/lafeuille/ljcp/rest/security/Auth0Configurer.java
@@ -3,15 +3,14 @@ package org.lafeuille.ljcp.rest.security;
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
 
-@ConditionalOnProperty({"auth0.api-audience", "auth0.issuer"})
-@Configuration
+@ConditionalOnProperty(prefix = "auth0", name = {"api-audience", "issuer"})
+@Component
 @Order(200)
-public class Auth0Config extends WebSecurityConfigurerAdapter {
+public class Auth0Configurer implements HttpSecurityConfigurer {
 
     @Value("${auth0.api-audience}")
     private String apiAudience;
@@ -20,7 +19,7 @@ public class Auth0Config extends WebSecurityConfigurerAdapter {
     private String issuer;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         JwtWebSecurityConfigurer
                 .forRS256(apiAudience, issuer)
                 .configure(http);
--- src/main/java/org/lafeuille/ljcp/rest/security/WebSecurityConfig.java
+++ src/main/java/org/lafeuille/ljcp/rest/security/WebSecurityConfig.java
@@ -1,18 +1,24 @@
 package org.lafeuille.ljcp.rest.security;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.List;
+
 @Configuration
-@ConditionalOnMissingBean(Auth0Config.class)
-@Order(300)
-public class BasicAuthConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final List<HttpSecurityConfigurer> configurers;
+
+    public WebSecurityConfig(List<HttpSecurityConfigurer> configurers) {
+        this.configurers = configurers;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        for (var configurer : configurers) {
+            configurer.configure(http);
+        }
     }
 }
