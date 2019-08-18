d647eb6283db3608f987bc1a40ba3338655f82fc
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2019-08-18 21:17:33 +0200

    Upgrade Spring Boot

---
+++ src/test/java/org/lafeuille/ljcp/ApplicationITCase.java
@@ -0,0 +1,38 @@
+package org.lafeuille.ljcp;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest("ljcp.event.filler.enabled=" + true)
+@ContextConfiguration(initializers = ApplicationITCase.Initializer.class)
+public class ApplicationITCase {
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer();
+
+    @Test
+    public void application() {
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext context) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword(),
+                    "spring.jpa.generate-ddl=true",
+                    "spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation=true"
+            ).applyTo(context.getEnvironment());
+        }
+    }
+
+}
